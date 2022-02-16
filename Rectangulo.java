e5e0f2309a7c52691a464f6ac960ab2705420e34
Luis <fireblacker1998@gmail.com>
2022-02-16 17:39:34 +0100

    Creacion de la clase Rectangulo y el metodo Area

---
+++ Rectangulo.java
@@ -0,0 +1,10 @@
+class Rectangulo  {
+    private double lado1,lado2;
+    public Rectangulo (int lado1,int lado2){
+        this.lado1 = lado1;
+        this.lado2 = lado2;
+    }
+    public int Area(int lado1,int lado2){
+        return lado1*lado2;
+    }
+}
