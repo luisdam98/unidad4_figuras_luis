2169ed0968bb715be9f9097ab1c55372db00f5dd
Luis <fireblacker1998@gmail.com>
2022-02-16 17:44:41 +0100

    Creacion del metodo perimetro

--- Rectangulo.java
+++ Rectangulo.java
@@ -1,10 +1,13 @@
 class Rectangulo  {
     private double lado1,lado2;
-    public Rectangulo (int lado1,int lado2){
+    public Rectangulo (double lado1,double lado2){
         this.lado1 = lado1;
         this.lado2 = lado2;
     }
-    public int Area(int lado1,int lado2){
+    public double Area(){
         return lado1*lado2;
+    }
+    public double perimetro(){
+        return (lado1*2)+ (lado2*2);
     }
 }
